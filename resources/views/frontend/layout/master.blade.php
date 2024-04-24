9342d15f86865039fff367a4838b8b6a8530c3ad
mohamed <medo85559@gmail.com>
2024-04-24 18:58:31 +0200

    changes

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -23,7 +23,8 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
         <!-- Libraries Stylesheet -->
-        <link href="{{ asset('frontend') }}/lib/animate/animate.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('') }}">
+        <link rel="stylesheet" href="{{ asset('frontend/lib/animate/animate.min.css') }}">
         <link href="{{ asset('frontend') }}/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
         <link href="{{ asset('frontend') }}/lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 
 Route::group(
