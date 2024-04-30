24515ad0cff7ae87d8999360f9853a6cd27a69fb
mohamed <medo85559@gmail.com>
2024-04-30 22:05:34 +0300

    changes

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -5,12 +5,11 @@
         <meta charset="UTF-8">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>General Dashboard &mdash; Stisla</title>
+        <title>Welcome</title>
 
         <!-- General CSS Files -->
         <link rel="stylesheet" href="{{ asset('backend/assets/modules/bootstrap/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('backend/assets/modules/fontawesome/css/all.min.css') }}">
-
 
         <!-- CSS Libraries -->
         <link rel="stylesheet" href="{{ asset('backend/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
